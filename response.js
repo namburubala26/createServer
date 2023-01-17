0f3a98d1bdaef255d0e2fa9740f5dd79eb155cb6
namburubala26 <115918201+namburubala26@users.noreply.github.com>
2023-01-17 21:25:51 +0530

    Add files via upload

---
+++ response.js
@@ -0,0 +1,20 @@
+const http = require('http');
+// function rqListener(req,res){
+
+// }
+// http.createServer(rqListener);
+// http.createServer(function(req,res){
+
+// });
+const server = http.createServer((req,res) => {
+    console.log(req.url, req.headers,req.methods);
+    // process.exit();
+    
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+    res.write('</html>');
+    res.end();
+});
+server.listen(3000);
